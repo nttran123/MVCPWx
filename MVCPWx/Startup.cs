b6f13ab4a52feaae7f7636093b2d1821fc770344
Thanh Tran <thanhtngcd17200@fpt.edu.vn>
2019-07-31 10:37:05 +0700

    Add project files.

---
+++ MVCPWx/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCPWx.Startup))]
+namespace MVCPWx
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
